3e950cfef99394b79f41e8dd8c9183f80fda795e
soxies1 <tyson_bulmer@hotmail.com>
2017-02-19 14:05:11 -0800

    Update with games and new viewing format

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ $(window).scroll(function() {
 	var imagePos = $(this).offset().top;
 
 	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
+		if (imagePos < topOfWindow+630) {
 			$(this).addClass("slideLeft");
 		}
 	});
@@ -37,7 +37,7 @@ $(window).scroll(function() {
 	var imagePos = $(this).offset().top;
 
 	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+630) {
+		if (imagePos < topOfWindow+400) {
 			$(this).addClass("slideUp");
 		}
 	});
@@ -74,12 +74,13 @@ $(".continue").click(function() {
 
 function hideOverlay(){
 	$('#overlay').fadeOut(100);
-	$('#playRaftIt').hide();
+	deselectHeader();
+	//$('#playRaftIt').hide();
 }
 
 function showOverlay(panel){
-	if(panel === games){
-		$('#playRaftIt').show();
+	if(panel === server){
+	 	selectHeader("#header-servers");
 	}
 
 	document.getElementById('carousel-img1').src = panel.images[0];
@@ -93,5 +94,63 @@ function showOverlay(panel){
         scrollTop: $("#overlay").offset().top
     }, 500);
 }
+
+function loadGameContent(){
+	var section = "#header-games";
+	selectHeader(section);
+
+	$("#aboutpanel").hide().load('includes/games.html').fadeIn('500');
+}
+
+function loadWebsiteContent(){
+	var section = "#header-websites";
+	selectHeader(section);
+
+	$("#aboutpanel").hide().load('includes/websites.html').fadeIn('500');
+}
+
+function loadHackathonContent(){
+	var section = "#header-hackathons";
+	selectHeader(section);
+
+	$("#aboutpanel").hide().load('includes/hackathons.html').fadeIn('500');
+}
+
+function returntomain(callback){
+	$("#aboutpanel").hide().load('includes/main.html').fadeIn('500', callback);
+	
+	deselectHeader();
+}
+
+function selectHeader(headervar){
+	deselectHeader();
+	$(headervar).animate({
+        borderWidth: 2
+    }, 200);
+
+    selectedCategory = headervar;
+}
+
+function deselectHeader(){
+	$(selectedCategory).animate({
+            borderWidth: 0
+        }, 200);
+
+	selectedCategory = null;
+}
+
+function showServerOverlay(){
+	if(selectedCategory == null){
+		showOverlay(server);
+	}else{
+		returntomain(serverSelect);
+	}
+}
+
+function serverSelect(){
+	showOverlay(server);
+}
+
+$(document).ready(returntomain());
 
 
--- js/variables.js
+++ js/variables.js
@@ -1,9 +1,4 @@
 //Panel sections text and images
-var games = {
-	title: "Games",
-	text: "What really sparked my passion for coding was a classic style text-based RPG I wrote in Java after my first year of university. Since then, I have done many projects with a good few being game related.",
-	images: ["img/carousel/games/textgame.png", "img/carousel/games/raft.png", "img/carousel/games/roll.png", "img/carousel/games/space.png"]
-}
 
 var hobbies = {
 	title: "Hobbies",
@@ -17,20 +12,10 @@ var education = {
 	images: ["img/carousel/education/uvic.jpg", "img/carousel/education/awards.jpg", "img/carousel/education/year.jpg","img/carousel/education/compsci.jpg"]
 }
 
-var websites = {
-	title: "Websites",
-	text: "Most of my experience so far has been in web development, I find myself frequently making websites for personal use or others.",
-	images: ["img/carousel/websites/bekitten.png", "img/carousel/websites/studybuddy.jpeg", "img/carousel/websites/modev.png","img/carousel/websites/questions.png"]
-}
-
-var roadAhead = {
-	title: "The Road Ahead",
-	text: "I have aspirations which involve traveling, learning, being the best at what I do, and always having fun.",
-	images: ["img/carousel/roadAhead/path.jpg", "img/carousel/roadAhead/learn.jpg", "img/carousel/roadAhead/map.jpg","img/carousel/roadAhead/fun.jpg"]
-}
-
 var server = {
 	title: "Home Server",
 	text: "Setting up a home server on a raspberry pi was one of my most practical and fun projects I have done so far. Aside from the money I've saved on hosting, the experience of working with LAMP and MEAN have been really valuable in my experience as a web developer.",
 	images: ["img/carousel/server/rasp.jpg", "img/carousel/server/lamp.jpg", "img/carousel/server/mean.jpg","img/carousel/server/ssh.png"]
 }
+
+var selectedCategory
