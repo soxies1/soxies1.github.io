2d3b877a419002931589e3878d2b142302c57e23
tysonbul <tyson_bulmer@hotmail.com>
2017-07-16 16:29:46 -0700

    Add AI hackathon and NSERC

--- js/custom.js
+++ js/custom.js
@@ -1,68 +1,67 @@
-
 $(window).scroll(function() {
-	$('#textgame').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#textgame').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
-			$(this).addClass("slideDown");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("slideDown");
+        }
+    });
 });
 
 $(window).scroll(function() {
-	$('#hobbies').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#hobbies').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+630) {
-			$(this).addClass("slideLeft");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 630) {
+            $(this).addClass("slideLeft");
+        }
+    });
 });
 
 $(window).scroll(function() {
-	$('#websites').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#websites').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
-			$(this).addClass("slideDown");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("slideDown");
+        }
+    });
 });
 
 $(window).scroll(function() {
-	$('#path').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#hackathons').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
-			$(this).addClass("slideUp");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 600) {
+            $(this).addClass("slideUp");
+        }
+    });
 });
 
 $(window).scroll(function() {
-	$('#homeserver').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#homeserver').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+550) {
-			$(this).addClass("slideUp");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 550) {
+            $(this).addClass("slideUp");
+        }
+    });
 });
 
 $(window).scroll(function() {
-	$('#education').each(function(){
-	var imagePos = $(this).offset().top;
+    $('#education').each(function() {
+        var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+600) {
-			$(this).addClass("slideUp");
-		}
-	});
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 600) {
+            $(this).addClass("slideUp");
+        }
+    });
 });
 
 $(".continue").click(function() {
@@ -72,83 +71,79 @@ $(".continue").click(function() {
 });
 
 
-function hideOverlay(){
-	$('#overlay').fadeOut(100);
-	deselectHeader();
+function hideOverlay() {
+    $('#overlay').fadeOut(100);
+    deselectHeader();
 }
 
-function loadGameContent(){
-	var section = "#header-games";
-	selectHeader(section);
+function loadGameContent() {
+    var section = "#header-games";
+    selectHeader(section);
 
-	$("#aboutpanel").hide().load('includes/games.html').fadeIn('500');
+    $("#aboutpanel").hide().load('includes/games.html').fadeIn('500');
 }
 
-function loadWebsiteContent(){
-	var section = "#header-websites";
-	selectHeader(section);
+function loadWebsiteContent() {
+    var section = "#header-websites";
+    selectHeader(section);
 
-	$("#aboutpanel").hide().load('includes/websites.html').fadeIn('500');
+    $("#aboutpanel").hide().load('includes/websites.html').fadeIn('500');
 }
 
-function loadHackathonContent(){
-	var section = "#header-hackathons";
-	selectHeader(section);
+function loadHackathonContent() {
+    var section = "#header-hackathons";
+    selectHeader(section);
 
-	$("#aboutpanel").hide().load('includes/hackathons.html').fadeIn('500');
+    $("#aboutpanel").hide().load('includes/hackathons.html').fadeIn('500');
 }
 
-function loadServerContent(){
-	var section = "#header-servers";
-	selectHeader(section);
+function loadServerContent() {
+    var section = "#header-servers";
+    selectHeader(section);
 
-	$("#aboutpanel").hide().load('includes/server.html').fadeIn('500');
+    $("#aboutpanel").hide().load('includes/server.html').fadeIn('500');
 }
 
-function loadEducationContent(){
-	document.getElementById('dialog-title').innerHTML = "Education";
-	$("#overlay-content").hide().load('includes/education.html').fadeIn('500');
-	$('#overlay').fadeIn(100);
-	$('html, body').animate({
+function loadEducationContent() {
+    var section = "#header-education";
+    selectHeader(section);
+
+    $("#aboutpanel").hide().load('includes/education.html').fadeIn('500');
+}
+
+function loadHobbiesContent() {
+    document.getElementById('dialog-title').innerHTML = "Hobbies";
+    $("#overlay-content").hide().load('includes/hobbies.html').fadeIn('500');
+    $('#overlay').fadeIn(100);
+    $('html, body').animate({
         scrollTop: $("#overlay").offset().top
     }, 500);
 }
 
-function loadHobbiesContent(){
-	document.getElementById('dialog-title').innerHTML = "Hobbies";
-	$("#overlay-content").hide().load('includes/hobbies.html').fadeIn('500');
-	$('#overlay').fadeIn(100);
-	$('html, body').animate({
-        scrollTop: $("#overlay").offset().top
-    }, 500);
-}
+function returntomain(callback) {
+    $("#aboutpanel").hide().load('includes/main.html').fadeIn('500', callback);
 
-function returntomain(callback){
-	$("#aboutpanel").hide().load('includes/main.html').fadeIn('500', callback);
-	
-	deselectHeader();
+    deselectHeader();
 }
 
 
 var selectedCategory;
 
-function selectHeader(headervar){
-	deselectHeader();
-	$(headervar).animate({
+function selectHeader(headervar) {
+    deselectHeader();
+    $(headervar).animate({
         borderWidth: 2
     }, 200);
 
     selectedCategory = headervar;
 }
 
-function deselectHeader(){
-	$(selectedCategory).animate({
-            borderWidth: 0
-        }, 200);
+function deselectHeader() {
+    $(selectedCategory).animate({
+        borderWidth: 0
+    }, 200);
 
-	selectedCategory = null;
+    selectedCategory = null;
 }
 
 $(document).ready(returntomain());
-
-
